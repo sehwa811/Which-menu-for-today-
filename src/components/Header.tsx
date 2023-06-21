bc5dc9a893d62efa079b654c34fb4dd17d74904c
sehwa811 <wsd08111@gmail.com>
2023-06-21 19:28:27 +0900

    changing manifest

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,5 @@ export default function Header() {
         <Card.Img src={"/images/food.jpg"} height="220px" className='img-responsive' />
         <span className="position-absolute text-white top-50 display-5" style={{}}>오늘 뭐 먹지?</span>
       </Card>
-
   );
 }
--- src/routes/MenuPage.tsx
+++ src/routes/MenuPage.tsx
@@ -10,7 +10,7 @@ export default function MenuPage() {
   return (
     <Container className="d-flex flex-column justify-content-center align-items-center mt-5">
       <span className="display-5">{data}</span>
-      <Link className="mt-4" to={""}>내 위치 근처 팟타이 맛집 찾아보기</Link>
+      <Link className="mt-4" to={""}>내 위치 근처 맛집 찾아보기</Link>
     </Container>
   );
 }
